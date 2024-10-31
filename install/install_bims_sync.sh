dc5366b2698250ff358b428c02bb0b4a2348423f
cartesv <cartesv@gmail.com>
2024-10-31 16:13:06 -0300

    Fix permisos rsync_key y unificación de scripts de instalación de rsync.

--- install/install_bims_sync.sh
+++ install/install_bims_sync.sh
@@ -2,6 +2,11 @@
 
 BOOT_SCRIPTS_PATH="/opt/install/bims-boot-scripts"
 
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Instalando key para rsync" >> /var/log/bims_boot.log
+rm -rf /root/rsync;
+cp -R $BOOT_SCRIPTS_PATH/rsync /root/rsync;
+chmod 600 /root/rsync/rsync_key
+
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Instalando comando bims_sync" >> /var/log/bims_boot.log
 cp -f $BOOT_SCRIPTS_PATH/sbin/bims_sync /usr/sbin/bims_sync;
 chmod 755 /usr/sbin/bims_sync;
--- install/install_rsync_key.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-BOOT_SCRIPTS_PATH="/opt/install/bims-boot-scripts"
-echo "$(date '+%Y-%m-%d %H:%M:%S') - Instalando key para rsync" >> /var/log/bims_boot.log
-rm -rf /root/rsync;
-cp -R $BOOT_SCRIPTS_PATH/rsync /root/rsync;
