bc3d14283283d7defe0f96b8aa340ed7f7811221
cartesv <cartesv@gmail.com>
2024-10-31 16:06:41 -0300

    Fix en install_bims_cron

--- install/install_bims_cron.sh
+++ install/install_bims_cron.sh
@@ -2,5 +2,5 @@
 
 BOOT_SCRIPTS_PATH="/opt/install/bims-boot-scripts"
 cp -f $BOOT_SCRIPTS_PATH/cron/bims_boot /etc/cron.d/;
-systemctl restart cron;
+systemctl restart crond;
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Se ha instalado el comando bims_boot" >> /var/log/bims_boot.log
