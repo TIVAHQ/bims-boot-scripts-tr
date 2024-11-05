5659a2aa94094110cf063f5cd6cb1ec091f2a9de
cartesv <cartesv@gmail.com>
2024-11-05 12:48:59 -0300

    Desinstalación de crons de g2

--- install/install_bims_cron.sh
+++ install/install_bims_cron.sh
@@ -2,5 +2,13 @@
 
 BOOT_SCRIPTS_PATH="/opt/install/bims-boot-scripts"
 cp -f $BOOT_SCRIPTS_PATH/cron/bims_boot /etc/cron.d/;
+
+# Se desinstalan crons que se ejecutarán directo en g2
+rm -f /etc/cron.d/getbims;
+rm -f /etc/cron.d/certbot;
+rm -f /etc/cron.d/saas_sama2;
+rm -f /etc/cron.d/backups_clientes;
+rm -f /etc/cron.d/gcloud;
+
 systemctl restart crond;
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Se ha instalado el comando bims_boot" >> /var/log/bims_boot.log
