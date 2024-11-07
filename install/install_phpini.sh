34dc8e6b02d809d0006c45abff353fcd91cb8d26
cartesv <cartesv@gmail.com>
2024-11-07 11:34:54 -0300

    Actualización de php.ini

---
+++ install/install_phpini.sh
@@ -0,0 +1,11 @@
+BOOT_SCRIPTS_PATH="/opt/install/bims-boot-scripts"
+
+# Verifica si hay diferencias entre /etc/php.ini y $BOOT_SCRIPTS_PATH/etc/php.ini
+if ! diff -q /etc/php.ini $BOOT_SCRIPTS_PATH/etc/php.ini &>/dev/null; then
+    echo "Actualizando /etc/php.ini..."
+    cp -f $BOOT_SCRIPTS_PATH/etc/php.ini /etc/php.ini
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - Se actualizó /etc/php.ini" >> $LOG_FILE
+    systemctl restart httpd
+else
+    echo "/etc/php.ini ya está actualizado."
+fi
--- install/install_redis.sh
+++ install/install_redis.sh
@@ -28,31 +28,31 @@ else
     echo "php-redis ya está instalado."
 fi
 
-# Verificar y configurar Redis como manejador de sesiones en php.ini
-if ! grep -q "^session.save_handler = redis$" /etc/php.ini; then
-    echo "Configurando php.ini para usar Redis como manejador de sesiones..."
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - Configurando php.ini para usar Redis como manejador de sesiones" >> $LOG_FILE
+# # Verificar y configurar Redis como manejador de sesiones en php.ini
+# if ! grep -q "^session.save_handler = redis$" /etc/php.ini; then
+#     echo "Configurando php.ini para usar Redis como manejador de sesiones..."
+#     echo "$(date '+%Y-%m-%d %H:%M:%S') - Configurando php.ini para usar Redis como manejador de sesiones" >> $LOG_FILE
 
-    # Asegurar que la configuración de session.save_handler exista y sea redis
-    if grep -q "^session.save_handler" /etc/php.ini; then
-        sed -i 's/^session.save_handler = .*/session.save_handler = redis/' /etc/php.ini
-    else
-        echo "session.save_handler = redis" >> /etc/php.ini
-    fi
+#     # Asegurar que la configuración de session.save_handler exista y sea redis
+#     if grep -q "^session.save_handler" /etc/php.ini; then
+#         sed -i 's/^session.save_handler = .*/session.save_handler = redis/' /etc/php.ini
+#     else
+#         echo "session.save_handler = redis" >> /etc/php.ini
+#     fi
 
-    # Asegurar que session.save_path esté configurado correctamente
-    if grep -q "^session.save_path" /etc/php.ini; then
-        sed -i 's|^session.save_path = .*|session.save_path = "tcp://'"$REDIS_HOST"':'"$REDIS_PORT"'"|' /etc/php.ini
-    else
-        echo 'session.save_path = "tcp://'"$REDIS_HOST"':'"$REDIS_PORT"'"' >> /etc/php.ini
-    fi
+#     # Asegurar que session.save_path esté configurado correctamente
+#     if grep -q "^session.save_path" /etc/php.ini; then
+#         sed -i 's|^session.save_path = .*|session.save_path = "tcp://'"$REDIS_HOST"':'"$REDIS_PORT"'"|' /etc/php.ini
+#     else
+#         echo 'session.save_path = "tcp://'"$REDIS_HOST"':'"$REDIS_PORT"'"' >> /etc/php.ini
+#     fi
 
-    # Señalar que se necesita reiniciar Apache
-    RESTART_NEEDED=1
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - Se aplicaron cambios en php.ini para Redis." >> $LOG_FILE
-else
-    echo "Redis ya está configurado como manejador de sesiones en php.ini."
-fi
+#     # Señalar que se necesita reiniciar Apache
+#     RESTART_NEEDED=1
+#     echo "$(date '+%Y-%m-%d %H:%M:%S') - Se aplicaron cambios en php.ini para Redis." >> $LOG_FILE
+# else
+#     echo "Redis ya está configurado como manejador de sesiones en php.ini."
+# fi
 
 # Archivo de configuración de Apache
 CONFIG_FILE="/etc/httpd/conf.d/php.conf"
