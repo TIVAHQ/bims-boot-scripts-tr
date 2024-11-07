87bb3a91aa8302056384b9ab28b6128efec18798
cartesv <cartesv@gmail.com>
2024-11-07 12:13:14 -0300

    Fix install bims_sync

--- install/install_bims_sync.sh
+++ install/install_bims_sync.sh
@@ -2,7 +2,7 @@
 
 BOOT_SCRIPTS_PATH="/opt/install/bims-boot-scripts"
 
-echo "$(date '+%Y-%m-%d %H:%M:%S') - Instalando key para rsync" >> /var/log/bims_boot.log
+# echo "$(date '+%Y-%m-%d %H:%M:%S') - Instalando key para rsync" >> /var/log/bims_boot.log
 # rm -rf /root/rsync;
 # Se crea el directorio /root/rsync si no existe
 # mkdir -p /root/rsync;
@@ -10,8 +10,8 @@ echo "$(date '+%Y-%m-%d %H:%M:%S') - Instalando key para rsync" >> /var/log/bims
 rm -rf /root/rsync/rsync;
 cp -f $BOOT_SCRIPTS_PATH/rsync/exclusions.txt /root/rsync/exclusions.txt;
 
-cp -R $BOOT_SCRIPTS_PATH/rsync /root/rsync;
-chmod 600 /root/rsync/rsync_key
+# cp -R $BOOT_SCRIPTS_PATH/rsync /root/rsync;
+# chmod 600 /root/rsync/rsync_key
 
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Instalando comando bims_sync" >> /var/log/bims_boot.log
 cp -f $BOOT_SCRIPTS_PATH/sbin/bims_sync /usr/sbin/bims_sync;
