ac171b517f5fbb004e5631e1f3816e48d60e1393
cartesv <cartesv@gmail.com>
2024-11-13 15:56:48 -0300

    Fix dir del watchdog de cloud storage

--- install/install_bims_check_cloud_storage.sh
+++ install/install_bims_check_cloud_storage.sh
@@ -3,5 +3,5 @@
 BOOT_SCRIPTS_PATH="/opt/install/bims-boot-scripts"
 
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Instalando watchdog del bucket de Google Cloud Storage" >> /var/log/bims_boot.log
-cp -f ./sbin/bims_check_cloud_storage /usr/sbin/bims_check_cloud_storage;
+cp -f $BOOT_SCRIPTS_PATH/sbin/bims_check_cloud_storage /usr/sbin/bims_check_cloud_storage;
 chmod 755 /usr/sbin/bims_check_cloud_storage;
