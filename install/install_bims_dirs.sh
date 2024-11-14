f50dc9a080574154ef60b692748706431ad4680b
cartesv <cartesv@gmail.com>
2024-11-14 11:27:14 -0300

    Se implementa el directorio de logs en el bucket

--- install/install_bims_dirs.sh
+++ install/install_bims_dirs.sh
@@ -3,3 +3,7 @@
 rm -rf /var/www/vhosts/bims.app/public/app/tmp/upload;
 ln -s /mnt/bims-bucket-1/saas/tmp/upload /var/www/vhosts/bims.app/public/app/tmp/upload;
 chmod 777 /var/www/vhosts/bims.app/public/app/tmp/upload;
+
+rm -rf /var/www/vhosts/bims.app/public/app/tmp/logs;
+ln -s /mnt/bims-bucket-1/saas/tmp/logs /var/www/vhosts/bims.app/public/app/tmp/logs;
+chmod 777 /var/www/vhosts/bims.app/public/app/tmp/logs;
