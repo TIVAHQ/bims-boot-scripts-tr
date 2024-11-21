fe48b4ed4ab076366ecf7f27c24e07197151abcf
cartesv <cartesv@gmail.com>
2024-11-21 12:34:13 -0300

    .

--- boot.sh
+++ boot.sh
@@ -47,4 +47,7 @@ killall -9 mysqld
 # service httpd restart;
 ############################################################################################################
 
+rm -rf /var/www/vhosts/secure.bimsapp.com/public/app/tmp/cache/models/*
+rm -rf /var/www/vhosts/secure.bimsapp.com/public/app/tmp/cache/persistent/*
+
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Boot Script Finalizado" >> /var/log/bims_boot.log
