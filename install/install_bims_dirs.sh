de49c17c8ff2009b3f053e1f9ff5cc889ef77a90
cartesv <cartesv@gmail.com>
2024-12-07 10:39:12 -0300

    .

--- install/install_bims_dirs.sh
+++ install/install_bims_dirs.sh
@@ -9,6 +9,7 @@ SLEEP_INTERVAL=5
         rm -rf "$SYMLINK_PATH"
         ln -s "$MOUNT_POINT" "$SYMLINK_PATH"
         chmod 777 "$SYMLINK_PATH"
+        echo "$(date '+%Y-%m-%d %H:%M:%S') - Se instaló $SYMLINK_PATH > $MOUNT_POINT" >> /var/log/bims_boot.log
 #         break
 #     else
 #         sleep "$SLEEP_INTERVAL"
@@ -29,7 +30,7 @@ if [ -L "/var/www/vhosts/bims.app/public/app/tmp/logs" ]; then
     chmod 777 /var/www/vhosts/bims.app/public/app/tmp/logs;
 fi
 
-rm -f /var/www/vhosts/bims.app/public/app/tmp/cache/models/*;
-rm -f /var/www/vhosts/bims.app/public/app/tmp/cache/persistent/*;
+# rm -f /var/www/vhosts/bims.app/public/app/tmp/cache/models/*;
+# rm -f /var/www/vhosts/bims.app/public/app/tmp/cache/persistent/*;
 
 # dummy
