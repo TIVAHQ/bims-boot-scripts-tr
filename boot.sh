98b220508fd602fc784a1b9f3624cf2dc3616521
cartesv <cartesv@gmail.com>
2024-12-10 00:13:07 -0300

    .

--- boot.sh
+++ boot.sh
@@ -7,32 +7,32 @@ echo "$(date '+%Y-%m-%d %H:%M:%S') - Boot Script Iniciado" >> $LOG_FILE
 
 ############################################################################################################
 # Se instala redis (incluir luego en el servidor modelo)
-bash $BOOT_SCRIPTS_PATH/install/install_redis.sh
+# bash $BOOT_SCRIPTS_PATH/install/install_redis.sh
 ############################################################################################################
 
 ############################################################################################################
 # Se actualiza php.ini
-bash $BOOT_SCRIPTS_PATH/install/install_phpini.sh
+# bash $BOOT_SCRIPTS_PATH/install/install_phpini.sh
 ############################################################################################################
 
 ############################################################################################################
 # Se instala el comando bims_sync
-bash $BOOT_SCRIPTS_PATH/install/install_bims_sync.sh
+# bash $BOOT_SCRIPTS_PATH/install/install_bims_sync.sh
 ############################################################################################################
 
 ############################################################################################################
 # Se instala el watchdog del bucket de Google Cloud Storage
-bash $BOOT_SCRIPTS_PATH/install/install_bims_check_cloud_storage.sh
+# bash $BOOT_SCRIPTS_PATH/install/install_bims_check_cloud_storage.sh
 ############################################################################################################
 
 ############################################################################################################
 # Se instala app/tmp/upload en Google Cloud Storage
-bash $BOOT_SCRIPTS_PATH/install/install_bims_dirs.sh
+# bash $BOOT_SCRIPTS_PATH/install/install_bims_dirs.sh
 ############################################################################################################
 
 ############################################################################################################
 # Se instala el actualizado del script de booteo
-bash $BOOT_SCRIPTS_PATH/install/install_bims_update_boot_scripts.sh
+# bash $BOOT_SCRIPTS_PATH/install/install_bims_update_boot_scripts.sh
 ############################################################################################################
 
 ############################################################################################################
