d639bea4b8a1dd83f57261469351e3f8e9c9dce5
cartesv <cartesv@gmail.com>
2025-03-20 12:47:36 -0300

    .

--- install/install_bims_update_boot_scripts.sh
+++ install/install_bims_update_boot_scripts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BOOT_SCRIPTS_PATH="/opt/install/bims-boot-scripts"
+BOOT_SCRIPTS_PATH="/opt/install/bims-boot-scripts-tr"
 
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Instalando comando bims_update_boot_scripts" >> /var/log/bims_boot.log
 cp -f $BOOT_SCRIPTS_PATH/sbin/bims_update_boot_scripts /usr/sbin/bims_update_boot_scripts;
