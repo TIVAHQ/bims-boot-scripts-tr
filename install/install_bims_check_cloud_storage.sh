e9ee18fffedad8a441cc8ed07abaa7f78ac0e6b8
cartesv <cartesv@gmail.com>
2025-03-20 16:50:27 -0300

    .

--- install/install_bims_check_cloud_storage.sh
+++ install/install_bims_check_cloud_storage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BOOT_SCRIPTS_PATH="/opt/install/bims-boot-scripts"
+BOOT_SCRIPTS_PATH="/opt/install/bims-boot-scripts-tr"
 
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Instalando watchdog del bucket de Google Cloud Storage" >> /var/log/bims_boot.log
 cp -f $BOOT_SCRIPTS_PATH/sbin/bims_check_cloud_storage /usr/sbin/bims_check_cloud_storage;
